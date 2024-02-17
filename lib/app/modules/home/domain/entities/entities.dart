91863e470569ac11e010bfbeb260a1b635cbceff
Julio Furtado <julio.furtado@terceirizadoraizen.com>
2024-02-16 22:19:24 -0400

    exports

---
+++ lib/app/modules/home/domain/entities/entities.dart
@@ -0,0 +1,7 @@
+export 'chart_entity.dart';
+export 'current_trading_period_entity.dart';
+export 'indicators_entity.dart';
+export 'meta_entity.dart';
+export 'quote_entity.dart';
+export 'result_entity.dart';
+export 'trading_periods_entity.dart';
